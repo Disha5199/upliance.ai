e9410fb18b5348f30f549eb29141eb3263caf1b8
Disha5199 <dishapardeshi1998@gmail.com>
2025-02-21 16:24:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Header from "./components/Header";
+import Dashboard from "./components/Dashboard";
+
+const MyComponent = () => {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ index.js
@@ -0,0 +1,28 @@
+// src/App.tsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
+import { Provider } from "react-redux";
+import store from "./store/store";
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
